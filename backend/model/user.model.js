ec7aebecef9f62d54022d3bf93e03612106fd962
keshrirohan <rohankeshri06@gmail.com>
2026-05-29 23:18:47 +0530

    feat: create user model with schema definition

---
+++ backend/model/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    },
+    
+    productList: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+        },
+    ],
+    orders: [
+        {
+            type: mongoose.Schema.Types.ObjectId,   
+            ref: "Order",
+        },
+    ],
+   
+    
+
+
+});
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
